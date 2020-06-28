2a81500f6a77d9db1cde0bf1776dff445c5ed113
Arman Hossain Antu <58564723+antu1024@users.noreply.github.com>
2020-06-28 05:57:02 -0700

    File

---
+++ AqT01.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+url=$1
+Green="\u001b[32m"
+Yellow="\u001b[33m"
+Blue="\u001b[34m"
+Magenta="\u001b[35m"
+Cyan="\u001b[36m"
+White="\u001b[37m"
+printf "${Green}"
+figlet -f big "                                AqT01"
+echo ""
+printf "${STOP}"
+echo "                    @| Script Name : AqT01                          |@"
+echo "                    @| All the commands of Aquatone together        |@"
+echo "                    @| Coded by ArMan HoSSa!n || Version: One Pieces|@"
+echo ""
+echo ""
+if [ ! -d "Report/$url" ];then
+	mkdir Report/$url
+fi
+if [ ! -d "Report/$url/discover" ];then
+	mkdir Report/$url/discover
+fi
+if [ ! -d "Report/$url/scan" ];then
+	mkdir Report/$url/scan
+fi
+if [ ! -d "Report/$url/takeover" ];then
+	mkdir Report/$url/takeover
+fi
+echo -e " ${Blue}             {+} Please make sure that your all Collector are running {+}"
+echo ""
+echo -e " ${Yellow}[+] Aquatone is discovering the domain of the site ..."
+aquatone-discover -d $url #| tee Report/$url/discover.txt
+mv /root/aquatone/$url/hosts.txt Report/$url/discover/
+mv /root/aquatone/$url/hosts.json Report/$url/discover/
+echo -e " ${Yellow}[+] Aquatone is scanning the domain of the site..."
+aquatone-scan -d $url #| tee Report/$url/scan.txt
+mv /root/aquatone/$url/open_ports.txt Report/$url/scan/
+mv /root/aquatone/$url/urls.txt Report/$url/scan/
+echo -e " ${Yellow}[+] Aquatone is trying to takeover the domain of the site..."
+aquatone-takeover -d $url #| tee Report/$url/takeover.txt
+mv /root/aquatone/creditkarma.com/takeovers.json Report/$url/takeover/
+echo ""
+echo -e " ${White}./Done :-)"
+echo -e " ${White}./peace :-)"
+echo ""
+echo -e " ${Cyan}                     @| Thanks for Using M3                     |@"
+echo -e " ${Magenta}                     @| Special Thanks [+] Aquatone Dev. Team   |@"
+echo -e " ${Blue}                     @| CopyRight © antu1024                    |@"
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+Red="\u001b[31m"
+Green="\u001b[32m"
+Yellow="\u001b[33m"
+echo ""
+echo -e "${Red}         #################         "
+echo -e "${Yellow}         #${Green}AqT01 Installer${Yellow}#         "
+echo -e "${Red}         #################          "
+echo ""
+if [ ! -d "Report" ];then
+	mkdir Report
+fi
+echo -e "${Green} [+] apt updating..."
+apt-get update >> update.txt
+rm update.txt
+echo -e "${Green} [#] Done."
+echo -e "${Red} [+] Installing RUBY..."
+apt-get install -y ruby >> ruby.txt
+rm ruby.txt
+apt-get install -y wpscan >> wp.txt
+rm wp.txt
+echo -e "${Red} [!] RUBY Installation DONE :-)"
+echo -e "${Yellow} [+] Installing AQUATONE..."
+gem install aquatone >> aquatone.txt
+rm aquatone.txt
+echo -e "${Yellow} [!] AQUATONE Installation DONE :-)"
+echo ""
+echo -e "${Green}            @# DONE @#          "
+echo ""
+#rm install.sh
